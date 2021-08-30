3d8c9b2a47f34399aea9700e481d35a35a2a5f80
Leila Moran <89357283+snapcat@users.noreply.github.com>
2021-08-30 19:50:49 -0400

    Update __init__.py

--- code/__init__.py
+++ code/__init__.py
@@ -2,4 +2,10 @@
 #from .test_me import inc
 #from .test_me import test_answer
 
-__all__ = ['test_me']
+#__all__ = ['test_me']
+
+def inc(x):
+    return x + 1
+
+def test_answer():
+    assert inc(3) == 4
