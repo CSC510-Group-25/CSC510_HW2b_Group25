8c984041e2ea19ace2b2d286257cf3a053097754
Leila Moran <89357283+snapcat@users.noreply.github.com>
2021-08-30 19:44:31 -0400

    Update __init__.py

--- code/__init__.py
+++ code/__init__.py
@@ -1,5 +1,5 @@
-import code.test_me
-from .test_me import inc
-from .test_me import test_answer
+#import code.test_me
+#from .test_me import inc
+#from .test_me import test_answer
 
-__all__ = ['inc', 'test_answer']
+__all__ = ['test_me']
