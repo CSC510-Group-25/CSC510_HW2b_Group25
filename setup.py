82ff88c945995b96228fcea7a501646cb20d1d9a
Leila Moran <89357283+snapcat@users.noreply.github.com>
2021-08-30 16:35:26 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,1 @@
-from setuptools import setup, find_packages
 
-setup(
-    name='HW2bGroup25',
-    url='https://github.com/snapcat/CSC510_HW2b_Group25',
-    author='Group 25',
-    author_email='none',
-    version='1.0.0',
-    packages=find_packages(where='code'),
-   # install_requires=['numpy'],
-    license='Apache-2.0',
-    description='i am a test',
-)
