e0062e84691576fb83def50312b5b6219af40426
Leila Moran <89357283+snapcat@users.noreply.github.com>
2021-08-31 14:41:09 -0400

    Create __init__.py

--- code/__init__.py
+++ code/__init__.py
@@ -1,11 +1,1 @@
-#import code.test_me
-#from .test_me import inc
-#from .test_me import test_answer
 
-#__all__ = ['test_me']
-
-def inc(x):
-    return x + 1
-
-def test_answer():
-    assert inc(3) == 4
