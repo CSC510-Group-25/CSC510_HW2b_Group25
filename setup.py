785d31b6c103cad4b6021290bf2bacc1d9869a34
ptaneja <ptaneja@ncsu.edu>
2021-09-01 14:45:57 -0400

    Mocking setup.py file

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-#from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
-#setup(
-#    name='HW2bGroup25',
-#    url='https://github.com/snapcat/CSC510_HW2b_Group25',
-#    author='Group 25',
-#    author_email='none',
-#    version='1.0.0',
-#    packages=['code'],
-#    install_requires=['numpy'],
-#    license='Apache-2.0',
-#    description='i am a test',
-#)
+setup(
+   name='HW2bGroup25',
+   url='https://github.com/snapcat/CSC510_HW2b_Group25',
+   author='Group 25',
+   author_email='none',
+   version='1.0.0',
+   packages=['code'],
+   install_requires=['pytest'],
+   license='Apache-2.0',
+   description='Howework_2b_CSC_510_Fall_2021',
+   install_requires = [
+       'pytest'
+   ],
+)
